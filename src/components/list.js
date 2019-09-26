ab79b1d90780a9d4a5cd32b5d2f3817a7c90478a
Colum Bennett <cbennett@redhat.com>
2019-09-26 15:05:06 +0200

    mock events service

--- src/components/list.js
+++ src/components/list.js
@@ -3,33 +3,26 @@ import React, { Component } from "react";
 import "antd/dist/antd.css";
 import { List, Typography } from 'antd';
 
-const data = [
-  'Test1',
-  'Test2',
-  'Test3',
-  'Test4',
-  'Test5',
-];
 
 export default class ListComponent extends Component {
-    render() {
-        return (
-            
-            <div>
-            
-            <List
-              header={<div>Header</div>}
-              footer={<div>Footer</div>}
-              bordered
-              dataSource={data}
-              renderItem={item => (
-                <List.Item>
-                  {item}
-                </List.Item>
-              )}
-            />
-            </div>
-            );
-        
-    }
+  render() {
+    return (
+
+      <div>
+
+        <List
+          header={<div>Header</div>}
+          footer={<div>Footer</div>}
+          bordered
+          dataSource={data}
+          renderItem={item => (
+            <List.Item>
+              {item}
+            </List.Item>
+          )}
+        />
+      </div>
+    );
+
+  }
 }
